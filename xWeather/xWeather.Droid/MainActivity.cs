a6f796941d93eedec32b3adf3a08f14ad145e466
zypher365 <ashimrajkonwar@gmail.com>
2017-03-26 17:48:09 +0530

    Made event

--- xWeather/xWeather.Droid/MainActivity.cs
+++ xWeather/xWeather.Droid/MainActivity.cs
@@ -7,12 +7,30 @@ namespace xWeather.Droid
     [Activity(Label = "xWeather.Droid", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+
+        private EditText cityName;
+        private Button getWetherButton;
+        
+
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
+
+            cityName = FindViewById<EditText>(Resource.Id.cityName);
+            getWetherButton = FindViewById<Button>(Resource.Id.getWether);
+
+
             // Set our view from the "main" layout resource
-            SetContentView (Resource.Layout.Main);
+            // SetContentView (Resource.Layout.Main);
+            getWetherButton.Click += GetWetherButton_Click;
+
+        }
+
+        private void GetWetherButton_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
